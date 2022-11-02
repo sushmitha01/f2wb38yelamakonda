fbb68a1f0199916d81af534aa7c5a0df0cee4f6e
sushmitha01 <yelamakondasushmitha01@gmail.com>
2022-11-02 17:50:57 -0500

    added images,gridbuild,selector,layout files

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # f2wb38yelamakonda
 # Sushmitha Yelamakonda
 hosted at https://f2wb38yelamakonda.onrender.com/
+
+Student <br>
+Name(String),
+Age(int),
+Address(String)
--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var StudentRouter = require('./routes/Student');
+var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Student',StudentRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
